d8f01b4f0ff8d08fb202e3af93307cd99afa45f4
davidtsangfoucs <davidtsangfoucs@gmail.com>
2023-06-12 17:25:04 +0800

    Finish detech Cat using Cat API

--- client/src/page/AddCat.js
+++ client/src/page/AddCat.js
@@ -1,148 +1,164 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import axioss from 'axios';
 import axios from '../commons/axios';
+import React, { useState, useEffect } from 'react';
 import Header from '../component/Header';
 import UseAuth from '../component/UseAuth';
 
+
 const AddCat = () => {
+    const { isLoggedIn, premission, userId, objId, userName, location } = UseAuth();
+
     const [cat, setCat] = useState({
         name: '',
         breed: '',
         age: '',
         description: '',
-        location: '',
+        location: location,
         image: '',
         ageUnit: 'Year'
     });
 
     const [imageSrc, setImageSrc] = useState();
-    const [uploadData, setUploadData] = useState();
     const [selectedFile, setSelectedFile] = useState(null);
     const [breed, setBreed] = useState('');
 
-    const { isLoggedIn, premission, userId, objId, location } = UseAuth();
-
-
     useEffect(() => {
+        // Set default location if available
+        // const location = ''; // Set the default location here
         if (location) {
-            setCat({ location: location })
+            setCat((prevCat) => ({ ...prevCat, location }));
         }
-    }, []);
+    }, [location]);
 
-    // check cat breed using cat API 
+    const handleChange = (e) => {
+        setCat({ ...cat, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Handle image upload
+        const form = e.currentTarget;
+        const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+        const formData = new FormData();
+
+        for (const file of fileInput.files) {
+            formData.append('file', file);
+        }
+
+        formData.append('upload_preset', 'web-api');
+
+        const data = await fetch('https://api.cloudinary.com/v1_1/dhhm4o35a/image/upload', {
+            method: 'POST',
+            body: formData
+        }).then((r) => r.json());
+
+        // Handle data
+        cat.age = cat.age + ' ' + cat.ageUnit;
+        cat.image = data.url;
+        // cat.location = ''; // Set the location value here
+
+        axios
+            .post('/add-cat', cat)
+            .then((response) => {
+                console.log(response.data);
+                // Post to social media
+
+                alert('Cat created successfully!'); // Show an alert message
+                setTimeout(() => {
+                    // Clear the form after 3 seconds
+                    setCat({
+                        name: '',
+                        breed: '',
+                        age: '',
+                        description: '',
+                        location: '', // Set the default location here
+                        image: '',
+                        ageUnit: 'Year'
+                    });
+                    setImageSrc('');
+                    window.location.reload();
+                }, 3000);
+            })
+            .catch((error) => {
+                console.error('There was an error!', error);
+            });
+    };
+
+    const handleOnChange = (changeEvent) => {
+        const reader = new FileReader();
+
+        reader.onload = function (onLoadEvent) {
+            setImageSrc(onLoadEvent.target.result);
+        };
+
+        reader.readAsDataURL(changeEvent.target.files[0]);
+    };
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         setSelectedFile(file);
     };
 
-    const handleBreedDetection = async (e) => {
+    const handleBreedDetection = async (event) => {
+        event.preventDefault();
 
-        e.preventDefault()
         try {
             const formData = new FormData();
             formData.append('file', selectedFile);
 
             const API_KEY = 'live_TrIi2B4etNkiDY9rWi0ZwPjbnmcymqek1orMpvlXhhCGyCagm1BNTfew7dDiIOmt';
-            const imageURL = `https://www.thesprucepets.com/thmb/uQnGtOt9VQiML2oG2YzAmPErrHo=/5441x0/filters:no_upscale():strip_icc()/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg`;
 
-
-
-            const breedResponse = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids&include_breeds=true&mime_types=jpg,png&limit=1&page=1&order=random&size=small&sub_id=demo-uniq-id&url=${imageURL}`, {
+            const uploadResponse = await axioss.post('https://api.thecatapi.com/v1/images/upload', formData, {
                 headers: {
-                    'x-api-key': API_KEY,
-                },
+                    'Content-Type': 'multipart/form-data',
+                    'x-api-key': API_KEY
+                }
+            });
+
+            const uploadedImageId = uploadResponse.data.id;
+
+            const breedResponse = await axioss.get(`https://api.thecatapi.com/v1/images/${uploadedImageId}/analysis`, {
+                headers: {
+                    'x-api-key': API_KEY
+                }
             });
 
             if (breedResponse.data) {
-                const detectedBreed = breedResponse.data[0].breeds[0].name;
-                setBreed(detectedBreed);
+                let detectedBreeds = [];
+
+                breedResponse.data.forEach((item) => {
+                    if (item.labels && item.labels.length > 0) {
+                        item.labels.forEach((label) => {
+                            const detectedBreed = {
+                                name: label.Name,
+                                confidence: label.Confidence,
+                            };
+                            detectedBreeds.push(detectedBreed);
+                        });
+                    }
+                });
+
+                if (detectedBreeds.length > 0) {
+                    setBreed(detectedBreeds);
+                } else {
+                    setBreed('Breed not found');
+                }
             } else {
                 setBreed('Breed not found');
             }
+
+
         } catch (error) {
             console.error('Error detecting cat breed:', error);
         }
     };
-    // end of API call by cat api 
-
-
-    const handleChange = e => {
-        setCat({ ...cat, [e.target.name]: e.target.value });
-    };
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-
-        // handle image 
-        const form = e.currentTarget;
-        const fileInput = Array.from(form.elements).find(({ name }) => name === 'file')
-        const formData = new FormData
-
-        for (const file of fileInput.files) {
-            formData.append('file', file)
-        }
-
-        formData.append('upload_preset', 'web-api')
-
-        const data = await fetch('https://api.cloudinary.com/v1_1/dhhm4o35a/image/upload', {
-            method: 'POST',
-            body: formData
-        }).then(r => r.json());
-        console.log('data', data)
-
-
-
-        // handle data 
-        cat.age = cat.age + " " + cat.ageUnit;
-        cat.image = data.url;
-        cat.location = location;
-
-        axios.post('/add-cat', cat)
-            .then(response => {
-                console.log(response.data);
-                // post to social media 
-
-                alert('Cat created successfully!'); // Show an alert message
-                setTimeout(() => { // Start a timer
-                    setCat({ // After 3 seconds, clear the form
-                        name: '',
-                        breed: '',
-                        age: '',
-                        description: '',
-                        location: { location },
-                        image: '',
-                        ageUnit: 'Year'
-                    });
-                    setImageSrc('')
-                    window.location.reload();
-                }, 3000);
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            });
-    };
-
-    function handleOnChange(changeEvent) {
-        const reader = new FileReader();
-
-        reader.onload = function (onLoadEvent) {
-            setImageSrc(onLoadEvent.target.result);
-            setUploadData(undefined);
-        }
-
-        reader.readAsDataURL(changeEvent.target.files[0]);
-    }
-
 
     return (
-
         <div className="add-cat">
-            <Header></Header>
+            <Header />
             <div className="container">
-                <form onSubmit={handleSubmit} >
+                <form onSubmit={handleSubmit}>
                     <div className="columns">
                         <div className="column">
                             <div className="field">
@@ -159,7 +175,7 @@ const AddCat = () => {
                                 </div>
                             </div>
 
-                            <div className="field ">
+                            <div className="field">
                                 <label className="label">Age</label>
                                 <div className="control dis-flex">
                                     <input className="input" type="number" name="age" value={cat.age} onChange={handleChange} required />
@@ -184,7 +200,7 @@ const AddCat = () => {
                             <div className="field">
                                 <label className="label">Location</label>
                                 <div className="control">
-                                    <input disabled={true} className="input" type="text" name="location" value={location} onChange={handleChange} required />
+                                    <input disabled={true} className="input" type="text" name="location" value={cat.location} onChange={handleChange} required />
                                 </div>
                             </div>
                             <div className="field">
@@ -192,26 +208,81 @@ const AddCat = () => {
                                 <p>
                                     <input onChange={handleOnChange} type="file" name="file" />
                                 </p>
+                                <img src={imageSrc} alt="Uploaded Cat" />
+                            </div>
+                            <div className="field">
+                                <label className="label">Image Upload</label>
+                                <div className="control">
+                                    <input onChange={handleFileChange} type="file" name="file" />
+                                    <button onClick={handleBreedDetection} disabled={!selectedFile}>Detect Breed</button>
+                                </div>
+                            </div>
+                            <div className="field">
+                                {/* <label className="label">Breed</label>
+                                <div className="control">
+                                    {Array.isArray(breed) && breed.length > 0 ? (
+                                        breed.map((item, index) => (
+                                            <div className='columns' key={index}>
+                                                <span className='column'>
+                                                    <div className="column">
+                                                        {item.name}
+                                                    </div>
+                                                </span>
+                                                <span className='column'> : {item.confidence.toFixed(2)}%</span>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <input className="input" type="text" name="breed" value={breed} disabled />
+                                    )}
+                                </div> */}
 
-                                <img src={imageSrc} />
+                                <article class="message is-success">
+                                    <div class="message-header">
+                                        <p>Breed</p>
+                                        <button class="delete" aria-label="delete"></button>
+                                    </div>
+                                    {/* <div className="field">
+                                        <label className="label">Breed</label>
+                                        <div className="control">
+                                            {Array.isArray(breed) && breed.length > 0 ? (
+                                                breed.map((item, index) => (
+                                                    <div key={index}>
+                                                        <span>{item.name}</span>:
+                                                        <span>Confidence {item.confidence.toFixed(2)}%</span>
+                                                    </div>
+                                                ))
+                                            ) : (
+                                                <input className="input" type="text" name="breed" value={breed} disabled />
+                                            )}
+                                        </div>
+                                    </div> */}
+                                    <table className='breed-table'>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th className='pd-left20'>Confidence</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody className="control">
+                                            {Array.isArray(breed) && breed.length > 0 ? (
+                                                breed.map((item, index) => (
+                                                    <tr key={index}>
+                                                        <td>{item.name}</td>
+                                                        <td className='pd-left20'>{item.confidence.toFixed(2)}%</td>
+                                                    </tr>
+                                                ))
+                                            ) : (
+                                                <div disabled value={breed} className="breed"></div>
+                                            )}
+                                        </tbody>
+
+
+
+                                    </table>
+                                </article>
                             </div>
-                            <div className="field">
-                                <label className="label">Image URL</label>
-                                <div className="control">
-                                    <input className="input" type="url" name="image" value={cat.image} onChange={handleChange} required />
-                                </div>
-                            </div>
-                            <div className="field">
-                                <label className="label">Breed</label>
-                                <div className="control">
-                                    <input className="input" type="text" name="breed" value={breed} disabled />
-                                </div>
-                            </div>
-                            <div className="field">
-                                <div className="control">
-                                    <button onClick={handleBreedDetection} disabled={!cat.image}>Detect Breed</button>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                     <div className="field">
